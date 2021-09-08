f793767715505acba013a1db603152fab5db1ab2
johanna.meier <johanna.meier@db-n.com>
2021-09-08 19:44:37 +0200

    PCOTY change background on click
    * transformed App & ColorCard to Components
    * added function change color to change background color of App on click on ColorCard-swatch

--- src/ColorCard.jsx
+++ src/ColorCard.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
-function ColorCard(card) {
-  return (
-    <div className="ColorCard">
-      <div className="ColorCard-swatch" style={{ backgroundColor: card.hex }}/>
-      <div className="ColorCard-data">
-        <p>Pantone</p>
-        <p>{card.code}</p>
-        <p>{card.name}</p>
+class ColorCard extends PureComponent {
+  render() {
+    const { card, onChangeColor } = this.props;
+    return (
+      <div className="ColorCard">
+        <div className="ColorCard-swatch" style={{ backgroundColor: card.hex }} onClick={() => onChangeColor(card.hex)}/>
+        <div className="ColorCard-data">
+          <p>Pantone</p>
+          <p>{card.code}</p>
+          <p>{card.name}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ColorCard;
